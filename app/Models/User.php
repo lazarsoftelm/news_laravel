0fb76d3e7fd8df35cb376feb7d87c40f344aa8d4
lazarsoftelm <lazar.kadic@softelm.com>
2022-10-28 17:04:05 +0200

    Comment added on User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Laravel\Sanctum\HasApiTokens;   // treba da se obrise nakon instalacije Passport modula za autentikaciju
 
 class User extends Authenticatable
 {
