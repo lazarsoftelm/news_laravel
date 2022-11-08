532b1f3834476309b75984c05ffb5f4c39f49dea
lazarsoftelm <lazar.kadic@softelm.com>
2022-11-08 10:18:41 +0100

    Comment removed

--- routes/web.php
+++ routes/web.php
@@ -71,23 +71,3 @@ Route::group(['middleware' => ['auth', 'web']], function () {
     Route::post('/subscribe/{user}', [UserController::class, 'storeSubscribe']);
 });
 
-// Route::get('/test_mail', function () {
-//     $email = new \SendGrid\Mail\Mail();
-//     $email->setFrom("vladanrstcmet@gmail.com", "Vladan Ristic");
-//     $email->setSubject("Sending with Twilio SendGrid is Fun");
-//     $email->addTo("lazarkadic@gmail.com", "Laza Kamikaza");
-//     $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
-//     $email->addContent(
-//         "text/html",
-//         "<strong>and easy to do anywhere, even with PHP</strong>"
-//     );
-//     $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
-//     try {
-//         $response = $sendgrid->send($email);
-//         print $response->statusCode() . "\n";
-//         print_r($response->headers());
-//         print $response->body() . "\n";
-//     } catch (Exception $e) {
-//         echo 'Caught exception: ' . $e->getMessage() . "\n";
-//     }
-// });
